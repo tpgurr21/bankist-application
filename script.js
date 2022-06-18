34127d476aaa0e433c500cfad14148d415d17c27
Thomas Gurr <tpgurr21@gmail.com>
2022-06-17 20:37:58 -0400

    committing again

--- script.js
+++ script.js
@@ -227,5 +227,6 @@ btnSort.addEventListener('click', function (e) {
   displayMovements(currentAccount.movements, !sorted);
   sorted = !sorted;
 });
+//////
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
